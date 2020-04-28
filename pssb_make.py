7ccb05dcffd88c30b8680ea16274a0b0898607fb
Hugo Potter <61484582+hpott@users.noreply.github.com>
2020-04-28 13:31:48 +1000

    Add support for favicons

--- pssb_make.py
+++ pssb_make.py
@@ -1,6 +1,6 @@
 import os
 n="\n"
-
+favicon=False
 id=raw_input("id: ")
 name=raw_input("name: ")
 url=raw_input("url: ")
@@ -9,7 +9,11 @@ cat=raw_input("catergory: ")
 browser=raw_input("browser(leave blank for default):  ")
 if browser == "":
     browser="firefox"
-imgfile=raw_input("Icon file: ")
+imgfile=raw_input("Icon file(leave blank to use site favicon): ")
+
+if imgfile == "":
+    print("Favicon will be generated when user installs")
+    favicon=True
 
 with open(id+".pssb","w+") as datafile:
     datafile.write(id+n+name+n+url+n+cat+n+browser+n+imgfile)
@@ -17,7 +21,9 @@ print('Building ssb file')
 data=id+".pssb"
 os.system("mkdir "+id)
 os.system("mv "+data+" "+id)
-os.system("mv "+imgfile+" "+id)
+if favicon==False:
+    os.system("mv "+imgfile+" "+id)
+
 os.system("tar -cvf "+id+".ssb "+id+"/")
 print("Cleaning up")
 os.system("rm "+id+"/*")
